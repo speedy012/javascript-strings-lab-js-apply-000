4085fa5a4a83fd57373787c9a327f8d5cfd5f958
Herman <hrivas5726@aol.com>
2018-11-07 00:57:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var greeting = "Hello, everybody!";
 
 var specialGuest = "Hello, Neil deGrasse Tyson!"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello," + specialGuest + "!";
 
 var topic = "space";
 
